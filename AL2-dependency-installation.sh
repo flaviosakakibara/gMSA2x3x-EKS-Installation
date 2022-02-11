a6246686902df1aedd10ac43f3f1b4dd0ef81537
Flavio Sakakibara <fsakakib@amazon.com>
2022-02-11 13:18:48 +0000

    Fixing privilege escalation for docker

--- AL2-dependency-installation.sh
+++ AL2-dependency-installation.sh
@@ -50,7 +50,7 @@ install_kustomize() {
     curl --silent --location --remote-name \
     "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.2.3/kustomize_kustomize.v3.2.3_linux_amd64" && \
     chmod a+x kustomize_kustomize.v3.2.3_linux_amd64 && \
-    sudo mv kustomize_kustomize.v3.2.3_linux_amd64 /usr/local/bin/kustomize
+    sudo mv kustomize_kustomize.v3.2.3_linux_amd64 /usr/local/sbin/kustomize
 }
 install_git() {
     echo "Installing Git"
